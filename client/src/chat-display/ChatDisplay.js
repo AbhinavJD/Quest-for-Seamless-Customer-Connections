cdfa12fd1ca0755efb5239e5cd68e801378e211c
Abhinav <35560638+AbhinavJD@users.noreply.github.com>
2024-02-09 13:28:14 +0000

    auto scroll fix

--- client/src/chat-display/ChatDisplay.js
+++ client/src/chat-display/ChatDisplay.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Messages from "../messages/Messages";
 
 export default function ChatDisplay(props) {
   const chatId = props.chatID;
   const messages = props.messagesToShow;
+  const chatDisplayRef = useRef(null);
+  // Scroll to the bottom when messages change
+  useEffect(() => {
+    chatDisplayRef.current.scrollTop = chatDisplayRef.current.scrollHeight;
+  }, [messages]);
   return (
-    <div className="flex-1 overflow-auto">
+    <div ref={chatDisplayRef} className="flex-1 overflow-auto">
       {messages?.map((message) => (
         <Messages
           message={message}
